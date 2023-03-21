c5316fcdc842cc10231b05f4b68ee8ff781b212a
jeongsangtae <wjdtkdxo55@naver.com>
2023-03-22 05:34:07 +0900

    Bulletin Board - HTML, CSS change

---
+++ public/scripts/write.js
@@ -0,0 +1,9 @@
+const form = document.querySelector(".write-container");
+const btnAdd = document.querySelector(".btn-add");
+
+function formWrite() {}
+
+function writeAdd() {}
+
+btnAdd.addEventListener("click", writeAdd);
+form.addEventListener("submit", formWrite);
