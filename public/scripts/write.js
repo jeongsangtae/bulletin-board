3b6f7d3b886721cebd19bf7da06f30bfa5053c59
jeongsangtae <wjdtkdxo55@naver.com>
2023-03-26 06:46:24 +0900

    Bulletin Board - localStorage fail...

--- public/scripts/write.js
+++ public/scripts/write.js
@@ -1,8 +1,10 @@
 const boardWriteForm = document.querySelector(".board-write-container");
-
-// const boardWriteInput = document.querySelector(
-//   ".board-write input, .board-write textarea"
-// );
+const boardTitle = document.querySelector(".board-title");
+const boardInfoNum = document.querySelector(".board-info-num");
+const boardInfoWriter = document.querySelector(".board-info-writer");
+const boardInfoDate = document.querySelector(".board-info-date");
+const boardInfoViews = document.querySelector(".board-info-views");
+const boardContent = document.querySelector(".board-content");
 const titleInput = document.querySelector("#title-input");
 const infoWriterInput = document.querySelector("#info-writer");
 const infoPasswordInput = document.querySelector("#info-password");
@@ -13,25 +15,40 @@ const btnAdd = document.querySelector(".btn-add");
 const BOARD_LISTS = "boardlists";
 let boardData = [];
 let date = new Date(); // 현재 시간
-const utcNow = date.getTime() + date.getTimezoneOffset() * 60 * 1000; // 현재 시간을 utc로 변환한 밀리세컨드값
-const koreaTime = 9 * 60 * 60 * 1000; // utc로 변환된 값을 한국 시간으로 변환시키기 위해 9시간(밀리세컨드)를 더함
-const koreaNow = new Date(utcNow + koreaTime); // utc로 변환된 값을 한국 시간으로 변환시키기 위해 9시간(밀리세컨드)를 더함
+let num = 0;
 
-let month = date.getUTCMonth() + 1; //months from 1-12
-let day = date.getUTCDate();
-let year = date.getUTCFullYear();
+date = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
 
-date = year + "." + month + "." + day;
+// let dateString = date.toLocaleDateString("ko-KR", {
+//   year: "numeric",
+//   month: "long",
+//   day: "numeric",
+// });7
 
-function dates() {
-  console.log(date);
-  console.log(koreaNow);
-}
-
-btnAdd.addEventListener("click", dates);
+// function boardListView () {
+//   const
+// }
 
 function saveBoardData() {
   localStorage.setItem(BOARD_LISTS, JSON.stringify(boardData));
+}
+
+function writeContents(newBoardObject) {
+  const title = document.createElement("div");
+  title.innerText = newBoardObject.title;
+  const ddNum = document.createElement("dd");
+  ddNum.innerText = newBoardObject.num;
+  const ddWriter = document.createElement("dd");
+  ddWriter.innerText = newBoardObject.writer;
+  const ddDate = document.createElement("dd");
+  ddDate.innerText = newBoardObject.date;
+  const ddContent = document.createElement("div");
+  ddContent.innerText = newBoardObject.content;
+  boardTitle.appendChild(title);
+  boardInfoNum.appendChild(ddNum);
+  boardInfoWriter.appendChild(ddWriter);
+  boardInfoDate.appendChild(ddDate);
+  // boardInfoViews.appendChild()
 }
 
 function writeAdd(event) {
@@ -46,21 +63,26 @@ function writeAdd(event) {
   saveInfoWriterInput.value = "";
   saveInfoPasswordInput.value = "";
   saveContentInfo.value = "";
+
   const newBoardObject = {
     title: saveTitleInput,
+    num: num++,
     writer: saveInfoWriterInput,
     password: saveInfoPasswordInput,
     content: saveContentInfo,
     date: date,
   };
   boardData.push(newBoardObject);
-  saveBoardData(boardListView);
+  writeContents(newBoardObject);
+  saveBoardData();
 }
 
 boardWriteForm.addEventListener("submit", writeAdd);
 
-if (saveBoardData !== null) {
-  const parsedBoardData = JSON.parse(saveBoardData);
+const savedBoardData = localStorage.getItem(BOARD_LISTS);
+
+if (savedBoardData !== null) {
+  const parsedBoardData = JSON.parse(savedBoardData);
   boardData = parsedBoardData;
   parsedBoardData.forEach(boardListView);
 }
