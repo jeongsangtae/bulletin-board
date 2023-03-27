d17916bd7b30d5fcceffb48e0e72b32846501115
jeongsangtae <wjdtkdxo55@naver.com>
2023-03-28 02:38:49 +0900

    Bulletin Board - boardlist divide, sort

--- public/scripts/write.js
+++ public/scripts/write.js
@@ -30,33 +30,51 @@ let linkWriteContent = "write-content.html";
 
 date = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
 
+// localStorage에 배열을 만들어서 데이터를 저장
 function saveBoardData() {
   localStorage.setItem(BOARD_LISTS, JSON.stringify(boardData));
 }
 
+// localStorage에 저장된 데이터를 가져와 게시판 목록에 보여주는 역할을 한다.
+// 그리고 보고싶은 게시물의 제목을 클릭하면 그 해당 내용을 보여주는 페이지로 이동한다.
 function boardLists(newBoardObject) {
-  const boardListViewNum = document.createElement("div");
-  boardListViewNum.innerText = newBoardObject.num;
-  const boardListViewWriter = document.createElement("div");
-  boardListViewWriter.innerText = newBoardObject.writer;
-  const boardListViewDate = document.createElement("div");
-  boardListViewDate.innerText = newBoardObject.date;
-  // const boardListViewCount = document.createElement("div");
-  // boardListViewCount.innerText = newBoardObject.count;
-  const a = document.createElement("a");
-  a.href = linkWriteContent;
-  const boardListViewTitle = document.createElement("div");
-  boardListViewTitle.innerText = newBoardObject.title;
-  boardListViewTitle.addEventListener("click", writeContents);
-  if (boardListNum && boardListTitle && boardListWriter && boardListDate) {
-    boardListNum.appendChild(boardListViewNum);
-    boardListWriter.appendChild(boardListViewWriter);
-    boardListDate.appendChild(boardListViewDate);
-    boardListTitle.appendChild(a);
-    a.appendChild(boardListViewTitle);
+  if (boardListTable !== null) {
+    const boardListViewTable = document.createElement("div");
+    boardListViewTable.classList.add("board-list-view-table");
+    boardListViewTable.id = newBoardObject.id;
+
+    const boardListViewNum = document.createElement("div");
+    boardListViewNum.classList.add("board-num");
+    boardListViewNum.innerText = newBoardObject.num;
+
+    const boardListViewWriter = document.createElement("div");
+    boardListViewWriter.classList.add("board-writer");
+    boardListViewWriter.innerText = newBoardObject.writer;
+
+    const boardListViewDate = document.createElement("div");
+    boardListViewDate.classList.add("board-date");
+    boardListViewDate.innerText = newBoardObject.date;
+    // const boardListViewCount = document.createElement("div");
+    // boardListViewCount.innerText = newBoardObject.count;
+    const boardListViewTitle = document.createElement("div");
+    boardListViewTitle.classList.add("board-title");
+    const a = document.createElement("a");
+    a.innerText = newBoardObject.title;
+    a.href = linkWriteContent;
+
+    boardListViewTitle.appendChild(a);
+    boardListViewTitle.addEventListener("click", writeContents);
+
+    boardListViewTable.appendChild(boardListViewNum);
+    boardListViewTable.appendChild(boardListViewTitle);
+    boardListViewTable.appendChild(boardListViewWriter);
+    boardListViewTable.appendChild(boardListViewDate);
+
+    boardListTable.appendChild(boardListViewTable);
   }
 }
 
+//
 function writeContents(newBoardObject) {
   const title = document.createElement("div");
   title.innerText = newBoardObject.title;
@@ -85,6 +103,8 @@ function writeContents(newBoardObject) {
   // boardInfoViews.appendChild()
 }
 
+// form안에 있는 input들을 가져와 내용을 작성하면 그걸 localStorage의 배열에 객체형식으로 저장한다.
+// 데이터가 저장되면 메인 페이지 즉, 게시글 목록이 있는 곳으로 돌아가도록 되어있다.
 function writeAdd(event) {
   event.preventDefault();
   const saveTitleInput = titleInput.value;
@@ -119,5 +139,7 @@ const savedBoardData = localStorage.getItem(BOARD_LISTS);
 if (savedBoardData !== null) {
   const parsedBoardData = JSON.parse(savedBoardData);
   boardData = parsedBoardData;
+  // num을 기준으로 내림차순 정렬
+  boardData.sort((a, b) => b.num - a.num);
   parsedBoardData.forEach(boardLists);
 }
